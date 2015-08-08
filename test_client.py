84ea0db4f7ea22f2eb72f9978cdbb6a313b0a128
Elvijs <elvijs@crowdsurfer.co>
2015-08-08 20:39:37 +0100

    A slightly less bad client

--- test_client.py
+++ test_client.py
@@ -8,9 +8,9 @@ import websockets
 def hello():
     websocket = yield from websockets.connect('ws://localhost:8765/')
     while True:
-        websocket.send('blah')
-        lat_lon_amount = yield from websocket.recv()
-        stuff = json.loads(lat_lon_amount)
-        pprint.pprint(stuff)
+        lat_lon_amount = (yield from websocket.recv())
+        if lat_lon_amount:
+            stuff = json.loads(lat_lon_amount)
+            pprint.pprint(stuff)
 
 asyncio.get_event_loop().run_until_complete(hello())
